f2d70855d3f8640ddf76e9c13d1928d81bbb277a
free2580 <fridakahsas@yahoo.com>
2018-11-14 17:06:59 -0500

    added transaction table

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,5 +1,5 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :sitter_id, :pet_id, :days_sat, :total_cost
+  attributes :id, :sitter_id, :pet_id, :days_sat, :total_cost, :start_date, :end_date
   belongs_to :pet
   belongs_to :sitter
 end
